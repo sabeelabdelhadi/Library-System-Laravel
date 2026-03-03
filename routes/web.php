4762ea279f81683e0c29e327bb4727fc9115a377
sabeelabdelhadi <sabeelabdelhadi@gmail.com>
2026-03-03 23:36:11 +0200

    fix(web.php): Redirect root URL to books index

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController; 
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::redirect('/', '/books');
 
 Route::resource('books', BookController::class);
